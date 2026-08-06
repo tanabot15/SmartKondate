055e042caf26bac5de13523a9415b3456bc42b44
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-06 22:49:22 +0900

    Create file structure 2

--- SmartKondate/SmartKondateApp.swift
+++ SmartKondate/SmartKondateApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SmartKondateApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainTabView()
         }
     }
 }
---
+++ SmartKondate/Views/MainTabView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/08/06.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainTabView()
+}
