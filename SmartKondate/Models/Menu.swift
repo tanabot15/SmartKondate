5297a3b5a1ae386ba054f760edf2dd05c0ab5cb5
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-06 22:54:31 +0900

    Create models files

---
+++ SmartKondate/Models/Menu.swift
@@ -0,0 +1,8 @@
+//
+//  Menu.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/08/06.
+//
+
+import Foundation
---
+++ SmartKondate/Models/PatternDay.swift
@@ -0,0 +1,8 @@
+//
+//  PatternDay.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/08/06.
+//
+
+import Foundation
