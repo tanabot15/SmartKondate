6a45141abda446bd53c9256cfa3961086f8e45dc
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-06 22:15:12 +0900

    Initial Commit

---
+++ SmartKondate/SmartKondateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartKondateApp.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/08/06.
+//
+
+import SwiftUI
+
+@main
+struct SmartKondateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
