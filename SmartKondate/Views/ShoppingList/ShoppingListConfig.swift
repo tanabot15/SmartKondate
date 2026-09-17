58c18a1ca5042142fbe6d8028d3c8b8f2e75cda0
tanabot15 <kenichirosuzuki1@gmail.com>
2026-09-17 15:33:49 +0900

    Improve ShoppingListView

---
+++ SmartKondate/Views/ShoppingList/ShoppingListConfig.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListConfig.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/09/17.
+//
+
+import Foundation
---
+++ SmartKondate/Views/ShoppingList/ShoppingSetupView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingSetupView.swift
+//  SmartKondate
+//
+//  Created by Kenichiro Suzuki on 2026/09/17.
+//
+
+import SwiftUI
+
+struct ShoppingSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingSetupView()
+}
